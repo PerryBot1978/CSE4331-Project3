ec0f970782704514c57c10e8943498c49b20fc02
Dillian Botello <dilliant.botello@gmail.com>
2025-12-01 14:31:12 -0600

    Some modifications

--- src/org/dtb9096/hadoop/job2/TitleCountReducer.java
+++ src/org/dtb9096/hadoop/job2/TitleCountReducer.java
@@ -9,6 +9,14 @@ import java.io.IOException;
 public class TitleCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private final IntWritable out_value = new IntWritable(0);
 
+    /**
+     * Reduces the (Year, List of 1s) to (Year, Sum)
+     * @param key The year in question
+     * @param values The list of 1s
+     * @param context The context the output will be written to
+     * @throws IOException Thrown by Context.write()
+     * @throws InterruptedException Thrown by Context.write()
+     */
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int count = 0;
         for (IntWritable val : values) {
