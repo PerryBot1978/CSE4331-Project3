28b8d102999611bfb62f7f6f8c176bbc698fc4f9
Dillian Botello <dilliant.botello@gmail.com>
2025-12-07 15:39:25 -0600

    Fix the \N problem

--- src/org/dtb9096/hadoop/job2/TitleCountReducer.java
+++ src/org/dtb9096/hadoop/job2/TitleCountReducer.java
@@ -9,6 +9,15 @@ import java.io.IOException;
 public class TitleCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private final IntWritable out_value = new IntWritable(0);
 
+    private static boolean isInt(String str) {
+        try {
+            Integer.parseInt(str);
+        }  catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
     /**
      * Reduces the (Year, List of 1s) to (Year, Sum)
      * @param key The year in question
@@ -18,6 +27,13 @@ public class TitleCountReducer extends Reducer<Text, IntWritable, Text, IntWrita
      * @throws InterruptedException Thrown by Context.write()
      */
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        // If the year is not a valid integer, don't do anything
+        // The given files seem to have an entry where the year is \N
+        // Which is not useful for this project
+        if (!isInt(key.toString())) {
+            return;
+        }
+
         int count = 0;
         for (IntWritable val : values) {
             count += val.get();
